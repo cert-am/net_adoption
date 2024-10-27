a5e9fe089dc8f70d56a3b208f9049993382df597
Tigran Antonyan <tigran@cert.am>
2024-10-27 18:46:36 +0400

    create design for your connection bar

--- lib/net_adoption_web/components/core_components.ex
+++ lib/net_adoption_web/components/core_components.ex
@@ -682,10 +682,10 @@ defmodule NetAdoptionWeb.CoreComponents do
   def domain_checker(%{domain: domain} = assigns) do
     ~H"""
     <form hx-get="/check" hx-target="#result" hx-swap="outerHTML" id="check" hx-indicator="#check-btn" class="flex justify-center">
-      <div class="w-full sm:w-[80%]">
+      <div class="w-full">
         <label for="hs-trailing-button-add-on-with-icon" class="sr-only">Label</label>
         <div class="flex justify-center items-center rounded-full shadow-2xl px-5 py-2">
-          <input type="text" name="domain" class="w-full border-none focus:ring-0" value={domain} required />
+          <input type="text" name="domain" placeholder="Ստոգել դոմեն" class="w-full border-none focus:ring-0" value={domain} required />
           <button
             id="check-btn"
             type="submit"
